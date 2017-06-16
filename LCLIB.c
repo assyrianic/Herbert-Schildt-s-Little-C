1ac96185b94cc4179397d4a4f926dddfdb2af590
assyrianic <assyrianic@users.noreply.github.com>
2017-06-15 19:24:25 -0500

    gussied up formatting
    changed up the formatting so it's easier on the eyes and more readable..

--- LCLIB.c
+++ LCLIB.c
@@ -10,90 +10,102 @@ extern char token[80]; /* holds string representation of token */
 extern char token_type; /* contains type of token */
 extern char tok; /* holds the internal representation of token */
 
-enum tok_types {DELIMITER, IDENTIFIER, NUMBER, KEYWORD,
-                TEMP, STRING, BLOCK};
+enum tok_types {
+	DELIMITER, IDENTIFIER, NUMBER, KEYWORD,
+	TEMP, STRING, BLOCK
+};
 
 /* These are the constants used to call sntx_err() when
-   a syntax error occurs. Add more if you like.
-   NOTE: SYNTAX is a generic error message used when
-   nothing else seems appropriate.
+	 a syntax error occurs. Add more if you like.
+	 NOTE: SYNTAX is a generic error message used when
+	 nothing else seems appropriate.
 */
-enum error_msg
-     {SYNTAX, UNBAL_PARENS, NO_EXP, EQUALS_EXPECTED,
-      NOT_VAR, PARAM_ERR, SEMI_EXPECTED,
-      UNBAL_BRACES, FUNC_UNDEF, TYPE_EXPECTED,
-      NEST_FUNC, RET_NOCALL, PAREN_EXPECTED,
-      WHILE_EXPECTED, QUOTE_EXPECTED, NOT_STRING,
-      TOO_MANY_LVARS};
+enum error_msg {
+	SYNTAX, UNBAL_PARENS, NO_EXP, EQUALS_EXPECTED,
+	NOT_VAR, PARAM_ERR, SEMI_EXPECTED,
+	UNBAL_BRACES, FUNC_UNDEF, TYPE_EXPECTED,
+	NEST_FUNC, RET_NOCALL, PAREN_EXPECTED,
+	WHILE_EXPECTED, QUOTE_EXPECTED, NOT_STRING,
+	TOO_MANY_LVARS
+};
 
 int get_token(void);
-void sntx_err(int error), eval_exp(int *result);
+void sntx_err(int error);
+void eval_exp(int *result);
 void putback(void);
 
 /* Get a character from console. (Use getchar() if
-   your compiler does not support getche().) */
+	 your compiler does not support getche().) */
 int call_getche()
 {
-  char ch;
-  ch = getchar();
-  while(*prog!=')') prog++;
-  prog++;   /* advance to end of line */
-  return ch;
+	char ch;
+	ch = getchar();
+	while( *prog!=')' )
+		prog++;
+	prog++;	 /* advance to end of line */
+	return ch;
 }
 
 /* Put a character to the display. */
 int call_putch()
 {
-  int value;
+	int value;
 
-  eval_exp(&value);
-  printf("%c", value);
-  return value;
+	eval_exp(&value);
+	printf("%c", value);
+	return value;
 }
 
 /* Call puts(). */
 int call_puts(void)
 {
-  get_token();
-  if(*token!='(') sntx_err(PAREN_EXPECTED);
-  get_token();
-  if(token_type!=STRING) sntx_err(QUOTE_EXPECTED);
-  puts(token);
-  get_token();
-  if(*token!=')') sntx_err(PAREN_EXPECTED);
+	get_token();
+	if( *token!='(' )
+		sntx_err(PAREN_EXPECTED);
+	get_token();
+	if( token_type!=STRING )
+		sntx_err(QUOTE_EXPECTED);
+	puts(token);
+	get_token();
+	if( *token!=')' )
+		sntx_err(PAREN_EXPECTED);
 
-  get_token();
-  if(*token!=';') sntx_err(SEMI_EXPECTED);
-  putback();
-  return 0;
+	get_token();
+	if( *token!=';' )
+		sntx_err(SEMI_EXPECTED);
+	putback();
+	return 0;
 }
 
 /* A built-in console output function. */
 int print(void)
 {
-  int i;
+	int i;
 
-  get_token();
-  if(*token!='(')  sntx_err(PAREN_EXPECTED);
+	get_token();
+	if( *token!='(' )
+		sntx_err(PAREN_EXPECTED);
 
-  get_token();
-  if(token_type==STRING) { /* output a string */
-    printf("%s ", token);
-  }
-  else {  /* output a number */
-   putback();
-   eval_exp(&i);
-   printf("%d ", i);
-  }
+	get_token();
+	if( token_type==STRING ) { /* output a string */
+		printf("%s ", token);
+	}
+	else {	/* output a number */
+		putback();
+		eval_exp(&i);
+		printf("%d ", i);
+	}
 
-  get_token();
+	get_token();
 
-  if(*token!=')') sntx_err(PAREN_EXPECTED);
+	if( *token!=')' )
+		sntx_err(PAREN_EXPECTED);
 
-  get_token();
-  if(*token!=';') sntx_err(SEMI_EXPECTED);
-  putback();
-  return 0;
+	get_token();
+	if( *token!=';' )
+		sntx_err(SEMI_EXPECTED);
+	putback();
+	return 0;
 }
 
 /* Read an integer from the keyboard. */
@@ -104,7 +116,7 @@ int getnum(void)
 	if( fgets(s, sizeof(s), stdin) ) {
 		while(*prog!=')')
 			prog++;
-		prog++;  /* advance to end of line */
+		prog++;	/* advance to end of line */
 		return strtol(s, NULL, 0);
 	}
 	return 0;
--- TEST.c
+++ TEST.c
@@ -1,59 +1,58 @@
 /* Little C Demonstration Program #1.
 
-   This program demonstrates all features
-   of C that are recognized by Little C.
+	 This program demonstrates all features
+	 of C that are recognized by Little C.
 */
 
-int i, j;   /* global vars */
+int i, j;	 /* global vars */
 char ch;
 
 // c++ comment
 
 main()
 {
-  int i, j;  /* local vars */
+	int i, j;	/* local vars */
 
-  puts("Little C Demo Program.");
+	puts("Little C Demo Program.");
 
-  print_alpha();
+	print_alpha();
 
-  do {
-    puts("enter a number (0 to quit): ");
-    i = getnum();
-    if(i < 0 ) {
-      puts("numbers must be positive, try again");
-    }
-    else {
-      for(j = 0; j < i; j=j+1) {
-        print(j);
-        print("summed is");
-        print(sum(j));
-        puts("");
-      }
-    }
-  } while(i!=0);
+	do {
+		puts("enter a number (0 to quit): ");
+		i = getnum();
+		if( i < 0 ) {
+			puts("numbers must be positive, try again");
+		}
+		else {
+			for( j=0 ; j<i ; j=j+1 ) {
+				print(j);
+				print("summed is");
+				print(sum(j));
+				puts("");
+			}
+		}
+	} while( i!=0 );
 }
 
 /* Sum the values between 0 and num. */
-sum(int num)
+int sum(int num)
 {
-  int running_sum;
+	int running_sum;
+	running_sum = 0;
 
-  running_sum = 0;
-
-  while(num) {
-    running_sum = running_sum + num;
-    num = num - 1;
-  }
-  return running_sum;
+	while( num ) {
+		running_sum = running_sum + num;
+		num = num - 1;
+	}
+	return running_sum;
 }
 
 /* Print the alphabet. */
-print_alpha()
+int print_alpha()
 {
-  for(ch = 'A'; ch<='Z'; ch = ch + 1) {
-    putch(ch);
-  }
-  puts("");
+	for(ch = 'A'; ch<='Z'; ch = ch + 1) {
+		putch(ch);
+	}
+	puts("");
 }
 
